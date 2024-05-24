c2fe737a13437a80a621b8f76c1a5fc700554b97
Swifter0 <167337586+Swifter0@users.noreply.github.com>
2024-05-24 17:30:27 +0200

    Rest add

---
+++ sb_squash_rest/src/main/java/hm/sb_squash_rest/SbSquashRestApplication.java
@@ -0,0 +1,13 @@
+package hm.sb_squash_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSquashRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSquashRestApplication.class, args);
+	}
+
+}
---
+++ sb_squash_rest/src/test/java/hm/sb_squash_rest/SbSquashRestApplicationTests.java
@@ -0,0 +1,13 @@
+package hm.sb_squash_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSquashRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
